f862a034cac17808ffc61848bc2b62d6d2cca5f3
RanKor <randomkorrektor@gmail.com>
2018-01-02 14:29:02 +0300

    Start project

---
+++ Serialize_Saber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer_Saber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
